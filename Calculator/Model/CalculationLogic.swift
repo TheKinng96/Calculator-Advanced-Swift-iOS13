fdeec77e2b424874307a190e9c527e430b8fb404
G3N <yap@corekara.co.jp>
2022-02-27 22:27:31 +0900

    finished the calculator

--- Calculator/Model/CalculationLogic.swift
+++ Calculator/Model/CalculationLogic.swift
@@ -10,12 +10,13 @@ import Foundation
 
 struct CalculationLogic {
     private var number: Double?
+    private var intermediateCalculation: (n1: Double, calcMethod: String)?
     
     mutating func setNumber(_ number: Double) {
         self.number = number
     }
     
-    func updateNumbers(with calcMethod: String) -> Double? {
+    mutating func updateNumbers(with calcMethod: String) -> Double? {
         if let n = number {
             switch calcMethod {
             case "+/-":
@@ -24,6 +25,26 @@ struct CalculationLogic {
                 return 0
             case "%":
                 return n * 0.01
+            case "=":
+                return calculate(n)
+            default:
+                intermediateCalculation = (n1: n, calcMethod: calcMethod)
+            }
+        }
+        return nil
+    }
+    
+    func calculate(_ n2: Double) -> Double? {
+        if let n1 = intermediateCalculation?.n1 {
+            switch intermediateCalculation?.calcMethod {
+            case "+":
+                return n2 + n1
+            case "-":
+                return n2 - n1
+            case "x":
+                return n2 * n1
+            case "÷":
+                return n1 / n2
             default:
                 return nil
             }
